20d776a9e0f510258778f8157f3bbdec5ea82192
Wesley Penachia <wesleyikiko@hotmail.com>
2021-09-18 17:04:38 -0300

    Service layer

---
+++ src/main/java/com/natymorgs/Curso/services/UserService.java
@@ -0,0 +1,26 @@
+package com.natymorgs.Curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.natymorgs.Curso.Entities.User;
+import com.natymorgs.Curso.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
