5a8cf5c56d2bbcf0d4e37673553cd14d75618472
Wesley Penachia <wesleyikiko@hotmail.com>
2021-09-18 19:32:41 -0300

    category

---
+++ src/main/java/com/natymorgs/Curso/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.natymorgs.Curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.natymorgs.Curso.Entities.Category;
+import com.natymorgs.Curso.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
