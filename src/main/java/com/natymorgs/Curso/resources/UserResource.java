00661f949be8f1e3851e79b62d88bd8b5361cbe8
Wesley Penachia <wesleyikiko@hotmail.com>
2021-09-18 15:39:50 -0300

    user entity and resouces

---
+++ src/main/java/com/natymorgs/Curso/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.natymorgs.Curso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.natymorgs.Curso.Entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "Maria@hotmail.com", "999999999", "1234");
+		return ResponseEntity.ok().body(u);
+	}
+}
